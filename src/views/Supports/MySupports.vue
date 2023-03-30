eb694f0575945cde0c735cae38914dca3cff1e78
Nathan <nathan48silva@gmail.com>
2023-03-30 14:57:11 -0300

    Criação do Mysupport

--- src/views/Supports/MySupports.vue
+++ src/views/Supports/MySupports.vue
@@ -1,14 +1,54 @@
 <template>
-    <div class="home">
-      Minhas dúvidas 
+    <div>
+    <div class="pageTitle">
+      <span class="title">Minhas Dúvidas</span>
+      <span class="dots">
+        <span></span>
+        <span></span>
+        <span></span>
+      </span>
     </div>
-  </template>
+
+    <div class="content">
+      <div class="container">
+        <div class="left">
+          <div class="card">
+            <div class="title bg-laravel">
+              <span class="text">Filtros</span>
+            </div>
+            <div class="modules">
+              <ul class="classes">
+                <li>Todos</li>
+                <li>Aguardando Minha Resposta</li>
+                <li>Aguardando Professor</li>
+                <li>Finalizados</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div class="right">
+          <div class="content">
+            <div class="comments">
+              <SupportsView />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
   
   <script>
-  
+  import SupportsView from "@/components/SupportsView.vue";
   
   export default {
     name: 'MySupports',
+
+    components: {
+      SupportsView,
+    }
   
   }
   </script>
---
+++ src/views/modules/ModulesAndLessons.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <div class="pageTitle">
+            <span class="title">Curso X</span>
+            <span class="dots">
+                <span></span>
+                <span></span>
+                <span></span>
+            </span>
+        </div>
+
+        <div class="content">
+            <div class="container">
+
+                <ModulesM/>
+
+                <div class="right">
+                    <div class="content">
+
+                        <PlayerM/>
+
+                        <SupportsM/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import ModulesM from './components/ModulesM.vue'
+import PlayerM from './components/PlayerM.vue'
+import SupportsM from './components/SupportsM.vue'
+export default {
+    name: 'ModulesAndLessons',
+    components: {
+        ModulesM,
+        PlayerM,
+        SupportsM,
+    }
+}
+</script>
